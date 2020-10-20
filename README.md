e37a63effeb1db4332394e2f848bddc622671110
Mark Music <markmusic999@gmailcom>
2020-10-19 21:15:50 -0600

    fix(README): 🔎 Updated README file

--- README.md
+++ README.md
@@ -3,8 +3,19 @@
 <br>
 <br>
 <br>
+<br>
+<br>
 
-# Permutate
+# Permutate Framework `1.0.11`
+
+Permutate is a JavaScripty/TypeScript and Dart framework to find every possible permutation of an array _(or list in dart)_ of data.
+
+Created with ❤️ by [@markmusic2727](https://twitter.com/MarkMusic2727) and [@filippofonseca](https://twitter.com/FilippoFonseca) 
+
+### __NPM: [link](https://twitter.com/MarkMusic2727)__
+### __PubDev: [link](https://twitter.com/MarkMusic2727)__
+
+## Why Permutate?
 
 
-
+## Initialization & Setup
--- dart/index.dart
+++ dart/index.dart
@@ -1,1 +1,37 @@
-String status = "havent started yet";
+class Permutate {
+  List arr = [];
+  Permutate({this.arr});
+  List output = [];
+
+  swapInPlace(arrToSwap, indexA, indexB) {
+    List temp = arrToSwap[indexA];
+    arrToSwap[indexA] = arrToSwap[indexB];
+    arrToSwap[indexB] = temp;
+  }
+
+  generate(n, List heapArr) {
+    if (n == 1) {
+      output.add(heapArr.sublist(0));
+      return;
+    }
+
+    generate(n - 1, heapArr);
+
+    for (int i = 0; i < n - 1; i++) {
+      n % 2 == 0
+          ? this.swapInPlace(heapArr, i, n - 1)
+          : this.swapInPlace(heapArr, 0, n - 1);
+      generate(n - 1, heapArr);
+    }
+  }
+
+  getPermutations() {
+    generate(arr.length, arr.sublist(0));
+    return output;
+  }
+}
+
+main() {
+  Permutate permutations = Permutate(arr: [1, 2]);
+  print(permutations.getPermutations());
+}
